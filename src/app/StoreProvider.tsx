aff0ac03f8d67c622d10af4d12840b84c7d0caf3
takbirgazi <takbirgazibd@gmail.com>
2025-06-17 23:55:48 +0600

    Chnage store and storeProvider

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,8 +1,17 @@
 "use client"
 import { store } from '@/lib/store'
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-    return <Provider store={store()}>{children}</Provider>
+    const persistor = persistStore(store);
+    return (
+        <Provider store={store}>
+            <PersistGate loading="loading..." persistor={persistor} >
+                {children}
+            </PersistGate>
+        </Provider>
+    )
 }
