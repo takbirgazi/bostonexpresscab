f51abb3e4a983a64b23e82f7e3e38a3765eb64ff
takbirgazi <takbirgazibd@gmail.com>
2025-06-25 18:23:15 +0600

    Update

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'apis.bostonexpresscab.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'admin.bostonexpresscab.com',
       },
     ],
--- src/components/BookingComponents/Charges/Charges.tsx
+++ src/components/BookingComponents/Charges/Charges.tsx
@@ -1,16 +1,23 @@
-
+"use client";
 import ChargeItem from "../ChargeItem/ChargeItem";
 import Image from "next/image";
-import car_1 from "@/assets/images/cars/car_1.jpg";
-import car_2 from "@/assets/images/cars/car_2.jpg";
-import car_3 from "@/assets/images/cars/car_3.jpg";
 import { BookingTypes } from "../BookingTypes";
+import { useEffect, useState } from "react";
 
 interface Charge {
     bookingData: BookingTypes
 }
 
 const Charges: React.FC<Charge> = ({ bookingData }) => {
+    const [carImage, setCarImage] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch(`https://apis.bostonexpresscab.com/api/car-photo/${(bookingData.vehicle_name).split(" ")[0]}`)
+            .then(response => response.json())
+            .then(data => {
+                setCarImage(("https://apis.bostonexpresscab.com/" + data.photo) || null);
+            });
+    }, [bookingData]);
 
     return (
         <div className="bg-white p-6 rounded-3xl shadow-md border-t-[6px] border-mainColor space-y-4">
@@ -18,7 +25,10 @@ const Charges: React.FC<Charge> = ({ bookingData }) => {
             {bookingData.uuid ? <>
                 <div className="rounded-2xl flex justify-center items-center">
                     <figure>
-                        <Image className="h-full max-h-40 rounded-md w-full object-cover" src={Number(bookingData.passenger) < 4 ? car_1 : (Number(bookingData.passenger) >= 4 && Number(bookingData.passenger) <= 6) ? car_2 : car_3} alt="Trip Summary" />
+                        {/* <Image className="h-full max-h-40 rounded-md w-full object-cover" src={Number(bookingData.passenger) < 4 ? car_1 : (Number(bookingData.passenger) >= 4 && Number(bookingData.passenger) <= 6) ? car_2 : car_3} alt="Trip Summary" /> */}
+                        {
+                            carImage ? <Image className="h-full max-h-40 rounded-md w-full object-cover" height={160} width={256} src={carImage} alt="Trip Summary" /> : ""
+                        }
                     </figure>
                 </div>
                 <div className="text-sm text-gray-700 space-y-1">
